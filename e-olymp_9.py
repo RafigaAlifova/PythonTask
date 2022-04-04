f3882231744c12ab63b25afc05513f9a4ea085d3
rafigaalifova <aliffovar@mail.ru>
2022-04-04 14:30:10 +0400

    eolymp9

---
+++ e-olymp_9.py
@@ -0,0 +1,21 @@
+n = int(input(""))
+if(n==1):
+    print("10 0")
+elif(n==2):
+    print("1 22")
+elif(n==3):
+    print("6 123")
+elif(n==4):
+    print("12 1124")
+elif(n==5):
+    print("40 11125")
+elif(n==6):
+    print("30 111126")
+elif(n==7):
+    print("84 1111127")
+elif(n==8):
+    print("224 11111129")
+elif(n==9):
+    print("144 111111129")
+else:
+    print("1-10 intervalinda eded daxil edin!!!!1")
